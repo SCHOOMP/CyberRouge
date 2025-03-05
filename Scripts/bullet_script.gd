b6bbdc890163c3f1af033ce63432ac428e4ab3a4
SCHOOMP <grahamschomp2@gmail.com>
2025-03-04 19:04:37 -0600

    Random Stuff

--- Scripts/bullet_script.gd
+++ Scripts/bullet_script.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
-@export var speed := 700  # Bullet speed
-var direction := Vector2.ZERO  # Movement direction
+@export var speed := 700 
+var damage
+var direction := Vector2.ZERO  
 
 func _process(delta):
 	if direction != Vector2.ZERO:
-		position += direction * speed * delta  # Move bullet forward
+		position += direction * speed * delta 
 
 func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()  # Remove bullet when it leaves the screen
+	queue_free()  
+
+	
